b1bd43c00430d8094242ad047df028dd218bc73a
junior-dalsasso <egidio.dalsasso@gmail.com>
2025-03-28 22:16:37 -0300

    [Window]
    Fix window with debug mode

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,31 +1,29 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/api.dart';
 import 'package:flutter_app/ecu_data_screen.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+// ignore: depend_on_referenced_packages
 import 'package:window_manager/window_manager.dart';
 
-// Talvez vai poder voltar para isso quando for testar na tela
-// void main() {
-//   API.updateBaseURI();
-//   runApp(const MyApp());
-// }
-
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   API.updateBaseURI();
 
-  await windowManager.ensureInitialized();
+  if (kDebugMode) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await windowManager.ensureInitialized();
+    WindowOptions windowOptions = const WindowOptions(
+      size: Size(800, 480),
+      center: true
+    );
 
-  WindowOptions windowOptions = const WindowOptions(
-    size: Size(800, 480),
-    center: true
-  );
-
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
-  });
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
 
   runApp(const MyApp());
 }
@@ -36,6 +34,7 @@ class MyApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.dark,
       darkTheme: ThemeData.dark().copyWith(textTheme: GoogleFonts.latoTextTheme().apply(bodyColor: Colors.white, displayColor: Colors.white)),
       initialRoute: '/dash',
       routes: {"/dash": (context) => const EcuDataScreen()},
